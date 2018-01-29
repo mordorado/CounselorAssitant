ab731a68494bd27e2a1dcea5672d60431305bb68
Mahmoud <mordorado@gmail.com>
2018-01-29 22:43:17 +0330

    .

--- CounselorAssistant/Controllers/PeopleController/PeopleController.cs
+++ CounselorAssistant/Controllers/PeopleController/PeopleController.cs
@@ -13,7 +13,7 @@ namespace CounselorAssistant.Controllers.PeopleController
         // GET: People
         public ActionResult Index()
         {
-            return View();
+            return View();      
         }
     }
 }
